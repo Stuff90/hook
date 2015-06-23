f8a45b884710cc12a9eeb04c8ca8f7fc00175cf8
Stuff90 <bernard.simon.90@gmail.com>
2015-06-23 17:04:42 +0200

    master  >  Make the whole thing to work

--- index.php
+++ index.php
@@ -9,35 +9,28 @@ use GitHubWebhook\Handler;
 
 $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 
-// --- storing an array ---
-// $test = array(
-//     "foo" => "bar",
-//     "i_love" => "lamp",
-//     "id" => 42
-// );
-// $dateTime = new DateTime();
-// $firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
+$handler = new Handler(SECRET, __DIR__);
+if($handler->handle()) {
+        $data = $handler->getData();
+        var_dump($data["repository"]);
 
-// // --- storing a string ---
-// $firebase->set(DEFAULT_PATH . '/name/contact001', "John Doe");
 
-// --- reading the stored string ---
-
-$projectName = "Documentation";
-$dayId 		 = date("dmY");
-$commitsPerPush = 2;
+        $projectName = $data["repository"]["name"];
+        $dayId           = date("dmY");
+        $commitsPerPush = sizeof($data["commits"]);
 
 
 
-$project = $firebase->get(DEFAULT_PATH . '/repo/' . $projectName);
-$day 	 = $firebase->get(DEFAULT_PATH . '/daily/' . $dayId);
+        $project = $firebase->get(DEFAULT_PATH . '/repo/' . $projectName);
+        $day     = $firebase->get(DEFAULT_PATH . '/daily/' . $dayId);
 
-if($project == "null") {
-	$project = $firebase->set(DEFAULT_PATH . '/repo/' . $projectName, 'test');
-}
+        if($project == "null") {
+                $project = $firebase->set(DEFAULT_PATH . '/repo/' . $projectName, 'test');
+        }
 
-if($day == "null") {
-	$firebase->set(DEFAULT_PATH . '/daily/' . $dayId , $commitsPerPush);
-} else {
-	$firebase->set(DEFAULT_PATH . '/daily/' . $dayId , $day + $commitsPerPush);
+        if($day == "null") {
+                $firebase->set(DEFAULT_PATH . '/daily/' . $dayId , $commitsPerPush);
+        } else {
+                $firebase->set(DEFAULT_PATH . '/daily/' . $dayId , $day + $commitsPerPush);
+        }
 }
