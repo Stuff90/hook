d3155fe0ee899562d215e5ff8e3f23ca33b9e234
Simon BERNARD <Stuff90@users.noreply.github.com>
2015-07-01 14:40:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,16 @@ use GitHubWebhook\Handler;
 
 $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 
+
+
 $handler = new Handler(SECRET, __DIR__);
 if($handler->handle()) {
         $data = $handler->getData();
-        var_dump($data["repository"]);
 
 
-        $projectName = $data["repository"]["name"];
-        $dayId           = date("dmY");
+        $userSender     = $data["sender"];
+        $projectName    = $data["repository"]["name"];
+        $dayId          = date("dmY");
         $commitsPerPush = sizeof($data["commits"]);
 
 
@@ -24,9 +26,52 @@ if($handler->handle()) {
         $project = $firebase->get(DEFAULT_PATH . '/repo/' . $projectName);
         $day     = $firebase->get(DEFAULT_PATH . '/daily/' . $dayId);
 
-        if($project == "null") {
-                $project = $firebase->set(DEFAULT_PATH . '/repo/' . $projectName, 'test');
+        foreach( $data["commits"] as $aCommit ) {
+                $aCommit['sender'] = $data["sender"];
+                $firebase->push( DEFAULT_PATH . '/history', $aCommit );
         }
+
+
+        if($project == "null") {
+                $dataToSave = array(
+                        $dayId => array(
+                                'total' => $commitsPerPush,
+                                'users' => array(
+                                        $userSender["login"] => $commitsPerPush
+                                )
+                        )
+                );
+
+                $firebase->set(DEFAULT_PATH . '/repo/' . $projectName , $dataToSave );
+
+        } else {
+
+                $projectAtDate = $firebase->get(DEFAULT_PATH . '/repo/' . $projectName . '/' . $dayId);
+
+                if($projectAtDate != "null") {
+
+                        $projectAtDateForUser = $firebase->get(DEFAULT_PATH . '/repo/' . $projectName . '/' . $dayId . '/users/' . $userSender["login"] );
+                        $projectAtDateTotal = $firebase->get(DEFAULT_PATH . '/repo/' . $projectName . '/' . $dayId . '/total' );
+                        $firebase->set(DEFAULT_PATH . '/repo/' . $projectName . '/' . $dayId . '/total' , $projectAtDateTotal + $commitsPerPush );
+
+                        if($projectAtDateForUser == "null") {
+                                $firebase->set(DEFAULT_PATH . '/repo/' . $projectName . '/' . $dayId . '/users/' . $userSender["login"] , $commitsPerPush );
+                        } else {
+                                $firebase->set(DEFAULT_PATH . '/repo/' . $projectName . '/' . $dayId . '/users/' . $userSender["login"] , $projectAtDateForUser + $commitsPerPush );
+                        }
+
+                } else {
+                        $data = array(
+                                'total' => $commitsPerPush,
+                                'users' => array(
+                                        $userSender["login"] => $commitsPerPush
+                                )
+                        );
+
+                        $firebase->set(DEFAULT_PATH . '/repo/' . $projectName . '/' . $dayId , $data );
+                }
+        }
+
 
         if($day == "null") {
                 $firebase->set(DEFAULT_PATH . '/daily/' . $dayId , $commitsPerPush);
